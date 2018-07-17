ddd0f2d339ec91af1cdaa48ec199b05103792adc
Arthur Edelstein <arthuredelstein@gmail.com>
2018-07-16 22:34:42 -0700

    Bug 26832: Allow cross-origin use of check.tpo/api/ip
    Allow the JSON data at https://check.torproject.org/api/ip
    to be fetched by a script in a web page or embedded in an
    iframe.

--- handlers.go
+++ handlers.go
@@ -103,6 +103,8 @@ type IPResp struct {
 func APIHandler(Exits *Exits) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/json")
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Del("X-Frame-Options")
 		var (
 			err   error
 			isTor bool
